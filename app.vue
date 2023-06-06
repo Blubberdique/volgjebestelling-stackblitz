f70942879098ad4bbba4cab9ef16f40208e0d7ab
Blubberdique <59876805+Blubberdique@users.noreply.github.com>
2023-06-06 14:04:48 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Components -->
+  <!-- !!!TAKE NOTE!!! -->
+  <!-- Navbar is, in fact, volgjebestelling -->
+  <Navbar />
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Navbar from './pages/navbar.vue';
+import MainPage from './pages/index.vue';
+
+export default {
+  components: {
+    Navbar,
+    MainPage,
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>The current year is:</h1>
+    <p>{{ currentYear }}</p>
+  </div>
+</template>
+
+<script>
+//
+// Empty line above is to be replaced by "import" statements
+
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
